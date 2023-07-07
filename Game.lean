698ef866238129c9c9e488fbabbbc177f38d1a61
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2023-07-07 16:06:46 +0100

    Update Game.lean

--- Game.lean
+++ Game.lean
@@ -1,42 +1,18 @@
 import GameServer.Commands
 
-import Game.Levels.Tutorial
-import Game.Levels.Addition
-import Game.Levels.Multiplication
-import Game.Levels.Power
-import Game.Levels.Function
-import Game.Levels.Proposition
-import Game.Levels.AdvProposition
-import Game.Levels.AdvAddition
-import Game.Levels.AdvMultiplication
---import Game.Levels.Inequality
+-- import Game.Levels.Tutorial
+-- import Game.Levels.Addition
+-- import Game.Levels.Multiplication
 
-Title "Natural Number Game"
+Title "Group Theory Game"
 Introduction
 "
-# Natural Number Game
+# The Group Theory Game
 
-##### version 3.0.2
+##### version 0.1
 
-*(note that this ported version of the NNG is still a bit rough around the edges
-and will experience some more love soon.)*
-
-Welcome to the natural number game -- a game which shows the power of induction.
-
-In this game, you get own version of the natural numbers, in an interactive
-theorem prover called Lean. Your version of the natural numbers satisfies something called
-the principle of mathematical induction, and a couple of other things too (Peano's axioms).
-Unfortunately, nobody has proved any theorems about these
-natural numbers yet! For example, addition will be defined for you,
-but nobody has proved that `x + y = y + x` yet. This is your job. You're going to
-prove mathematical theorems using the Lean theorem prover. In other words, you're going to solve
-levels in a computer game.
-
-You're going to prove these theorems using *tactics*. The introductory world, Tutorial World,
-will take you through some of these tactics. During your proofs, the assistant shows your
-\"goal\" (i.e. what you're supposed to be proving) and keeps track of the state of your proof.
-
-Click on the blue \"Tutorial World\" to start your journey!
+Imperial College students trying to figure out if it's possible to make
+a game out of group theory.
 
 ## Save progress
 
@@ -47,20 +23,17 @@ If you delete it, your progress will be lost!
 
 ## Credits
 
-* **Original Lean3-version:** Kevin Buzzard, Mohammad Pedramfar
-* **Content adaptation**: Jon Eugster
-* **Game Engine:** Alexander Bentkamp, Jon Eugster, Patrick Massot
+There is no code yet.
 
 ## Resources
 
 * The [Lean Zulip chat](https://leanprover.zulipchat.com/) forum
-* [Original Lean3 version](https://www.ma.imperial.ac.uk/~buzzard/xena/natural_number_game/)
-* [A textbook-style (lean4) version of the NN-game](https://lovettsoftware.com/NaturalNumbers/TutorialWorld/Level1.lean.html)
+* [Mathematics In Lean](https://leanprover-community.github.io/mathematics_in_lean/).
 
 "
 
-Path Tutorial → Addition → Function → Proposition → AdvProposition → AdvAddition → AdvMultiplication
-Path Addition → Multiplication → AdvMultiplication -- → Inequality
-Path Multiplication → Power
+--Path Tutorial → Addition → Function → Proposition → AdvProposition → AdvAddition → AdvMultiplication
+--Path Addition → Multiplication → AdvMultiplication -- → Inequality
+--Path Multiplication → Power
 
 MakeGame
